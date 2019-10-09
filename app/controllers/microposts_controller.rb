5d9da7062b1e7b00b217fa7d6802aae7c1320c9a
Noah-Bytelion <noah.day@bytelion.com>
2019-10-09 15:56:35 -0400

    Bugs no more
    Fixed several errors including the flash messages producing extra code

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -7,7 +7,7 @@ before_action :correct_user, only: :destroy
   def create
     @micropost = current_user.microposts.build(micropost_params)
     if @micropost.save
-      flash[:success] = "Micropost created!"
+      flash[:success] = 'Micropost created!'
       redirect_to root_url
     else
       @feed_items = []
@@ -17,17 +17,18 @@ before_action :correct_user, only: :destroy
 
   def destroy
     @micropost.destroy
-    flash[:success] = "Micropost deleted"
+    flash[:success] = 'Micropost deleted'
     redirect_to request.referrer || root_url
   end
 
   private
-  def micropost_params
-    params.require(:micropost).permit(:content, :picture)
-  end
 
-  def correct_user
-    @micropost = current_user.microposts.find_by(id: params[:id])
-    redirect_to root_url if @micropost.nil?
-  end
+    def micropost_params
+      params.require(:micropost).permit(:content)
+    end
+
+    def correct_user
+      @micropost = current_user.microposts.find_by(id: params[:id])
+      redirect_to root_url if @micropost.nil?
+    end
 end
