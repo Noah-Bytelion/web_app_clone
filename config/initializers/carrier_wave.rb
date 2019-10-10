925f13a5d5285fa9a3343f4fa161a8496c9c1d21
Noah-Bytelion <noah.day@bytelion.com>
2019-10-10 13:38:39 -0400

    round 2
    updated the carrier file

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -3,12 +3,10 @@ if Rails.env.production?
     config.root = Rails.root.join('tmp') # adding these...
     config.cache_dir = 'carrierwave' # ...two lines
     config.fog_credentials = {
-      :provider               => 'AWS',                        # required
-      :s3_access_key_id      => ENV['S3_ACCESS_KEY'],                        # required
-      :s3_secret_access_key  => ENV['S3_SECRET_KEY'],                     # required
-      :region                 => 'eu-west-1',                  # optional, defaults to 'us-east-1'
-      :host                   => 's3.example.com',             # optional, defaults to nil
-      :endpoint               => 'https://s3.example.com:8080' # optional, defaults to nil
+      #configuration for Amazon S3
+      :provider => 'AWS',
+      :aws_access_key_id  => ENV['S3_ACCESS_KEY'],
+      :aws_secret_access_key  => ENV['S3_ACCESS_KEY']
     }
     config.fog_directory = ENV['S3_BUCKET']
 #    config.fog_provider = 'fog/aws'
@@ -19,11 +17,3 @@ end
 # $heroku config:set S3_ACCESS_KEY=<access key>
 # $heroku config:set S3_SECRET_KEY=<secret key>
 # $heroku config:set S3_BUCKET=<bucket name>
-
-
-
-# 
-#   config.fog_directory  = ENV['S3_Bucket']                             # required
-#   config.fog_public     = false                                   # optional, defaults to true
-#   config.fog_attributes = {'Cache-Control'=>'max-age=315576000'}  # optional, defaults to {}
-# end
